008ece53a3a3865d0e3a9d558b6485f574919c6d
gbalejandro <alejandrobright@gmail.com>
2018-12-02 11:58:34 -0500

    Commit inicial del curso

---
+++ EFCore_Mod3/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Mod3.Models
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=EFCore_Mod3_Udemy;Integrated Security=True")
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(new LoggerFactory().AddConsole((category, level) => level == LogLevel.Information && category == DbLoggerCategory.Database.Command.Name, true));
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
---
+++ EFCore_Mod3/Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Mod3.Models
+{
+    class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
