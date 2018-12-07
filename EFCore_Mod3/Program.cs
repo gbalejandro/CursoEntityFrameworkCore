8bb8d9afe40e7990ebff08b1b83ca36c28fbf2dc
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-12-07 10:35:58 -0500

    Se agregaron nuevos scripts con expresiones lambda.

--- EFCore_Mod3/Program.cs
+++ EFCore_Mod3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_Mod3.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,28 +33,88 @@ namespace EFCore_Mod3
             //    });
             //}
 
-            using (var context = new ApplicationDbContext())
-            {
-                //// paso 2: notificamos que queremos agregar un estudiante
-                //context.AddRange(estudiantes);
+            //using (var context = new ApplicationDbContext())
+            //{
+            //// paso 2: notificamos que queremos agregar un estudiante
+            //context.AddRange(estudiantes);
 
-                //// paso 3: Guardamos los cambios
-                //context.SaveChanges();
-                //var estudiantes = context.Estudiantes.ToList();
-                //var estudiantes2 = context.Estudiantes.OrderByDescending(x => x.FechaNacimiento)
-                //    .ThenBy(x => x.Nombre).ToList();
+            //// paso 3: Guardamos los cambios
+            //context.SaveChanges();
+            //var estudiantes = context.Estudiantes.ToList();
+            //var estudiantes2 = context.Estudiantes.OrderByDescending(x => x.FechaNacimiento)
+            //    .ThenBy(x => x.Nombre).ToList();
 
-                // Da error si no hay registros en la tabla
+            // Da error si no hay registros en la tabla
 
-                //var estudiantes = context.Estudiantes.Select(x => x.Nombre).FirstOrDefault();
+            //var estudiantes = context.Estudiantes.Select(x => x.Nombre).FirstOrDefault();
 
-                // Proyección a un tipo anónimo
-                //var studentsTipoAnonimo = context.Estudiantes.Select(x => new { x.Id, x.Nombre }).ToList();
+            // Proyección a un tipo anónimo
+            //var studentsTipoAnonimo = context.Estudiantes.Select(x => new { x.Id, x.Nombre }).ToList();
 
-                // Proyección a una clase
-                var studentsEnClase = context.Estudiantes
-                    .Select(x => new EstudianteViewModel { Id = x.Id, Nombre = x.Nombre }).ToList();
-            }
+            // Proyección a una clase
+            //var studentsEnClase = context.Estudiantes
+            //    .Select(x => new EstudianteViewModel { Id = x.Id, Nombre = x.Nombre }).ToList();
+
+            //var estudiante = context.Estudiantes.Where(x => x.Id == 5).FirstOrDefault();
+            //var estudiante = context.Estudiantes.FirstOrDefault(x => x.Id == 5);
+
+            //var estudiantesMayores = context.Estudiantes
+            //    .Where(x => x.FechaNacimiento <= DateTime.Today.AddYears(-30) || x.Id == 5).ToList();
+
+            // Modelo conectado
+            //var student = context.Estudiantes.First(x => x.Nombre.StartsWith("Estudiante 0"));
+            //student.Nombre += " Gonzalez";
+            //context.SaveChanges();
+            //}
+
+            //// Modelo desconectado
+            //Estudiante claudia;
+
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    claudia = context.Estudiantes.First(x => x.Nombre.StartsWith("Estudiante 2"));
+            //}
+
+            //claudia.Nombre += " Purcell";
+
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    // se actualizan todas las columnas
+            //    //context.Entry(claudia).State = EntityState.Modified;
+            //    // se especifica que columna se actualiza
+            //    var entrada = context.Attach(claudia);
+            //    entrada.Property(x => x.Nombre).IsModified = true;
+            //    context.SaveChanges();
+            //}
+
+            // eliminar data
+            // Modelo conectado
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    var estudiante1 = context.Estudiantes.FirstOrDefault();
+            //    if (estudiante1 != null)
+            //    {
+            //        Console.WriteLine($"Estudiante a ser removido: { estudiante1.Nombre }");
+            //        context.Remove(estudiante1);
+            //        context.SaveChanges();
+            //    }
+            //}
+
+            // Modelo desconectado
+            //var studentId = 0;
+
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    studentId = context.Estudiantes.Select(x => x.Id).First();
+            //}
+
+            //using (var context = new ApplicationDbContext())
+            //{
+            //    var student = new Estudiante();
+            //    student.Id = studentId;
+            //    context.Entry(student).State = EntityState.Deleted;
+            //    context.SaveChanges();
+            //}
         }
     }
 
